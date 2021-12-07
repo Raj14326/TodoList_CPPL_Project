d78720ae0b47978bf6d7194b33f0c87a2198f844
YourDad07 <salmanadhikari415@gmail.com>
2021-12-07 13:20:48 +0530

    Added the date

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 $(function () {
     $("#datepicker").datepicker();
 });
+function adddatepicker(id) {
+    $("#" + id).datepicker();
+};
+function ValidateData(text, date) {
+    if (text === '' || date === '') {
+        alert("Please enter all the fields");
+        return false;
+    }
+    else {
+        return true;
+    }
+}
 var data = [];
 try {
     var x = JSON.parse(localStorage.getItem('todos'));
@@ -17,16 +29,22 @@ catch (e) {
     console.log('op');
 }
 const todoItemsList = document.querySelector('.todoitems');
-function func() {
+function Submit() {
     const task = document.getElementById("task").value;
-    var todo = {
-        id: Date.now(),
-        name: task,
-        completed: false
-    };
-    data.push(todo);
-    addToLocalStorage(data)
-    document.getElementById("task").value = ''
+    const date = document.getElementById('datepicker').value;
+    console.log(date);
+    if (ValidateData(task, date)) {
+        var todo = {
+            id: Date.now(),
+            name: task,
+            completed: false,
+            Expires: date
+        };
+        data.push(todo);
+        addToLocalStorage(data)
+
+        document.getElementById("task").value = ''
+    }
 }
 function renderTodos(todos) {
     todoItemsList.innerHTML = '';
@@ -46,10 +64,13 @@ function renderTodos(todos) {
         <i class="fa fa-check-square-o"></i>
         </span>
         </button></div>
-     <div class="col-7">
+     <div class="col-5">
       <p class="text-center  font-monospace fs-4 text-white "  id="${item.id + 'P'}"  "><del>${item.name}</del></p></div>
+      <div class="col-2">
+                  <p class="text-center  font-monospace fs-4 text-white " id="${item.id + 'D'}"><del>${item.Expires}</del></p>
+                  </div>
       <div class="col-3">
-      <button type="button" id="${item.id + 'del'}" class="ibtn" onclick="delete_me(${item.id})"> <span>
+      <button type="button" id="${item.id + 'del'}" class="ibtn ps-3" onclick="delete_me(${item.id})"> <span>
       <i class="fa fa-trash"></i>
       </span></button>
       <button type="button" id="${item.id + 'ed'}" class="ibtn" onclick="edit(${item.id})"><span>
@@ -74,10 +95,13 @@ function renderTodos(todos) {
                 <i class="fa fa-square-o"></i>
                 </span>
                 </button></div>
-                <div class="col-7">
+                <div class="col-5">
                   <p class="text-center  font-monospace fs-4 text-white "  id="${item.id + 'P'}">${item.name}</p></div>
+                  <div class="col-2">
+                  <p class="text-center  font-monospace fs-4 text-white " id="${item.id + 'D'}">${item.Expires}</p>
+                  </div>
                   <div class="col-3">
-                    <button type="button" id="${item.id + 'del'}" class="ibtn" onclick="delete_me(${item.id})">
+                    <button type="button" id="${item.id + 'del'}" class="ibtn ps-3" onclick="delete_me(${item.id})">
                     <span>
                     <i class="fa fa-trash"></i>
                     </span>
@@ -140,14 +164,17 @@ function check_me(id) {
 getFromLocalStorage();
 function edit(id) {
     var my_text = document.getElementById(id + "ed").className
+    console.log("--->>> ", date_t1);
     if (my_text === 'ibtn edit') {
-
         console.log("ok");
         var t = document.getElementById(id + "P").innerHTML
         var text = $('#' + id + "P").text();
+        var date_t1 = document.getElementById(id + "D").innerHTML
         console.log("--->>> ", text);
         var input = $(`<input type="text" class="edit text " aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg" id="${id + 'in'}" value="${text}" />`)
         document.getElementById(id + "indicate").className = "fa fa-save"
+        $('#' + id + 'D').replaceWith(`<input type="text" class="edit text" id="${id + 'Date'}" value="${date_t1}"></input>`)
+        adddatepicker(id + 'Date')
         $('#' + id + "P").replaceWith(input);
         document.getElementById(id + "ed").className = "ibtn Save"
         console.log(text);
@@ -155,21 +182,26 @@ function edit(id) {
     if (my_text === 'ibtn Save') {
         console.log("oks");
         var text = $('#' + id + "in").val();
+        var date_text = $('#' + id + "Date").val();
+        console.log('-->> D', date_text);
         console.log(text);
-        var temp = JSON.parse(localStorage.getItem('todos'));
-        for (let i = 0; i < temp.length; i++) {
-            if (id === temp[i].id) {
-                var todo = {
-                    id: id,
-                    name: text,
-                    completed: false
-                };
-                data[i] = todo;
-                addToLocalStorage(data)
+        if (ValidateData(text, date_text)) {
+            var temp = JSON.parse(localStorage.getItem('todos'));
+            for (let i = 0; i < temp.length; i++) {
+                if (id === temp[i].id) {
+                    var todo = {
+                        id: id,
+                        name: text,
+                        completed: false,
+                        Expires: date_text
+                    };
+                    data[i] = todo;
+                    addToLocalStorage(data)
+                }
             }
+            var input = $(`<p class="text-center  font-monospace fs-4 text-white" id="${id + 'P'}">${text}</p>`)
+            $('#' + id + "in").replaceWith(input);
+            document.getElementById(id + "ed").className = "ibtn edit"
         }
-        var input = $(`<p class="text-center  font-monospace fs-4 text-white" id="${id + 'P'}">${text}</p>`)
-        $('#' + id + "in").replaceWith(input);
-        document.getElementById(id + "ed").className = "ibtn edit"
     }
 }
